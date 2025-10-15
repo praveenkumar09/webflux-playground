f4df80ad28a237dc4ab6a3eaa27b882c99b1aa44
praveenkumar09 <tpkprav@gmail.com>
2025-10-15 20:19:35 +0800

    Added reactive and traditional controller to get products from external service

---
+++ src/main/java/com/praveen/webflux/webflux_playground/sec01/Product.java
@@ -0,0 +1,6 @@
+package com.praveen.webflux.webflux_playground.sec01;
+
+public record Product(Integer id,
+                      String description,
+                      Integer price) {
+}
---
+++ src/main/java/com/praveen/webflux/webflux_playground/sec01/TraditionalWebController.java
@@ -0,0 +1,35 @@
+package com.praveen.webflux.webflux_playground.sec01;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/traditional")
+public class TraditionalWebController {
+    private static final Logger log = LoggerFactory.getLogger(TraditionalWebController.class);
+    private final RestClient restClient = RestClient
+            .builder()
+            .baseUrl("http://localhost:7070")
+            .build();
+
+
+    @GetMapping("/products")
+    public List<Product> getProducts(){
+        List<Product> list = this.restClient
+                .get()
+                .uri("/demo01/products")
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+        log.info("Products: {}", list);
+        return list;
+    }
+
+}
