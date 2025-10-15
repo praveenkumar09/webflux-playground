4266959d9b98bb14019d24c2a84e0017163ea377
praveenkumar09 <tpkprav@gmail.com>
2025-10-15 20:01:22 +0800

    Initial Project

---
+++ src/test/java/com/praveen/webflux/webflux_playground/WebfluxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.webflux.webflux_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
