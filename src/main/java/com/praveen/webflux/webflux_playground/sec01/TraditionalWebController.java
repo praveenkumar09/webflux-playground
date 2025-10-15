1745c27089367359f9d0facdf1d7f2dc06f436df
praveenkumar09 <tpkprav@gmail.com>
2025-10-15 21:01:20 +0800

    Added code on how nopt to use reactive

--- src/main/java/com/praveen/webflux/webflux_playground/sec01/TraditionalWebController.java
+++ src/main/java/com/praveen/webflux/webflux_playground/sec01/TraditionalWebController.java
@@ -1,12 +1,14 @@
 package com.praveen.webflux.webflux_playground.sec01;
 
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -18,22 +20,33 @@ public class TraditionalWebController {
     private static final Logger log = LoggerFactory.getLogger(TraditionalWebController.class);
     private final RestClient restClient = RestClient
             .builder()
+            .requestFactory(new JdkClientHttpRequestFactory())
             .baseUrl("http://localhost:7070")
             .build();
 
 
     @GetMapping("/products")
-    public Mono<List<Product>> getProducts() {
-        return Mono.fromCallable(() -> {
-            List<Product> list = this.restClient
-                    .get()
-                    .uri("/demo01/products")
-                    .retrieve()
-                    .body(new ParameterizedTypeReference<List<Product>>() {
-                    });
-            log.info("Products: {}", list);
-            return list;
-        })
-                .subscribeOn(Schedulers.boundedElastic());
+    public List<Product> getProducts() {
+        List<Product> products = this.restClient
+                .get()
+                .uri("/demo01/products")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<Product>>() {
+                });
+        log.info("Products: {}", products);
+        return products;
+    }
+
+    @Deprecated
+    @GetMapping("/notreactive/products")
+    public Flux<Product> getProductsNotReactive(){
+        List<Product> products = this.restClient
+                .get()
+                .uri("/demo01/products")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<Product>>() {
+                });
+        log.info("Products: {}", products);
+        return Flux.fromIterable(products);
     }
 }
