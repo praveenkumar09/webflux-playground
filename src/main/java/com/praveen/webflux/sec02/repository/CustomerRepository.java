b2290ab40d5e59b5f7c56219369a395f422184f1
praveenkumar09 <tpkprav@gmail.com>
2025-10-17 21:23:22 +0800

    Created Customer Repository memory and test for all methods

--- src/main/java/com/praveen/webflux/sec02/repository/CustomerRepository.java
+++ src/main/java/com/praveen/webflux/sec02/repository/CustomerRepository.java
@@ -2,6 +2,16 @@ package com.praveen.webflux.sec02.repository;
 
 import com.praveen.webflux.sec02.entity.Customer;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+
+    Flux<Customer> findByEmail(String email);
+
+    Flux<Customer> findByName(String name);
+
+    Flux<Customer> findByNameAndEmail(String name, String email);
+
+    Flux<Customer> findByEmailEndingWith(String email);
 }
