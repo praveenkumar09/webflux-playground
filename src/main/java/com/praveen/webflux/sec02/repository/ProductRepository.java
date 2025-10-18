6d604a0349616e8b4ab5519246ec938182b913cc
praveenkumar09 <tpkprav@gmail.com>
2025-10-18 10:51:32 +0800

    Added repository and test method to PageableOfProducts

--- src/main/java/com/praveen/webflux/sec02/repository/ProductRepository.java
+++ src/main/java/com/praveen/webflux/sec02/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.praveen.webflux.sec02.repository;
 
 import com.praveen.webflux.sec02.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -10,4 +11,6 @@ import reactor.core.publisher.Flux;
 public interface ProductRepository extends ReactiveCrudRepository<Product, Integer> {
 
     Flux<Product> findByPriceBetween(Integer min, Integer max);
+
+    Flux<Product> findBy(Pageable pageable);
 }
--- src/test/java/com/praveen/webflux/tests/sec02/Lec02ProductRepositoryTest.java
+++ src/test/java/com/praveen/webflux/tests/sec02/Lec02ProductRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.praveen.webflux.tests.sec02;
 
 import com.praveen.webflux.sec02.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import reactor.test.StepVerifier;
 
 public class Lec02ProductRepositoryTest extends AbstractTest{
@@ -17,8 +20,26 @@ public class Lec02ProductRepositoryTest extends AbstractTest{
     public void testFindByPriceBetween() {
         this.productRepository
                 .findByPriceBetween(100,1000)
+                .doOnNext(product -> log.info("Product: {}", product))
                 .as(StepVerifier::create)
                 .expectNextCount(7)
+                .verifyComplete();
+    }
+
+    @Test
+    public void findByPageable(){
+        this.productRepository
+                .findBy(PageRequest.of(0,
+                        3)
+                        .withSort(Sort
+                                .by("price")
+                                .ascending())
+                )
+                .doOnNext(product -> log.info("Product: {}", product))
+                .as(StepVerifier::create)
+                .assertNext(product -> Assertions.assertEquals(200, product.getPrice()))
+                .assertNext(product -> Assertions.assertEquals(250, product.getPrice()))
+                .assertNext(product -> Assertions.assertEquals(300, product.getPrice()))
                 .verifyComplete();
     }
 
